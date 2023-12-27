47ddf8e025608373a39478de37877af703533d58
suraphon65 <suraphon.th.65@ubu.ac.th>
2023-12-27 15:47:33 +0700

    index.html

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function redirectToSamePage() {
   // หรือใช้ window.open() เพื่อเปิดหน้าเว็บใหม่ในหน้าเดียวกันหรือหน้าใหม่
   // window.open(window.location.href, '_blank');
 }
+
+const createSnowflake = () => {
+  const snowflake = document.createElement('div');
+  snowflake.classList.add('snowflake');
+  snowflake.style.left = `${Math.random() * window.innerWidth}px`;
+  snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
+  document.querySelector('.snowflakes').appendChild(snowflake);
+  setTimeout(() => {
+    snowflake.remove();
+  }, 5000);
+};
+
+setInterval(createSnowflake, 200);
